d1b4bf2d48b7a123c81b18ae9496587e28e847fd
Biggy <igor1606553@gmail.com>
2020-07-30 11:26:14 +0200

    Added proxy support; Downloaded books are stored in directory named year and week number

--- getmybooks.py
+++ getmybooks.py
@@ -1,11 +1,34 @@
 import requests
 from bs4 import BeautifulSoup
+import os.path
+import datetime
 
 homepage = 'http://books.goalkicker.com/'
 
+use_proxy = False
+
+http_proxy  = "http://10.10.1.10:3128"
+https_proxy = "https://10.10.1.11:1080"
+ftp_proxy   = "ftp://10.10.1.10:3128"
+
+r = requests.get(url, headers=headers, proxies=proxyDict)
+
+if use_proxy is True:
+	proxyDict = { 
+				"http"  : http_proxy, 
+				"https" : https_proxy, 
+				"ftp"   : ftp_proxy
+				}
+else:
+	proxyDict = {}
+
+today = datetime.date.today()
+today_iso = today.isocalendar()
+save_path = str(today_iso[0]) + str(today_iso[1]) + '/'
+
 with requests.session() as s:
 
-	home = s.get(homepage)
+	home = s.get(homepage, proxies=proxyDict)
 	bowl = BeautifulSoup(home.content, 'html.parser')
 
 	# get the URLS for each book subpage from the homepage
@@ -20,11 +43,11 @@ with requests.session() as s:
 			book_url = homepage + i['href']
 			book_urls.append(book_url)
 
-	# get book download link from each subpage and download it ;-)
+	# get book download link from each subpage and download it
 
 	for book_front in book_urls:
 
-		book_site = s.get(book_front)
+		book_site = s.get(book_front, proxies=proxyDict)
 		book_code = BeautifulSoup(book_site.content, 'html.parser')
 
 		for dummy in book_code.find_all('div', {'id': 'frontpage'}):
@@ -38,7 +61,9 @@ with requests.session() as s:
 					download_link = download_payload
 
 					print('Downloading ' + link['href'] + ' ...')
+					file = s.get(download_link, proxies=proxyDict)
 
-					file = s.get(download_link)
-					with open(link['href'], 'wb') as f:
+					if os.path.isdir(save_path) is False:
+							os.mkdir(save_path)
+					with open(save_path + link['href'], 'wb') as f:
 						f.write(file.content)
